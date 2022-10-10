51349a013d923add06b7e3aa19a0b0a07ccbd19f
Vinicio Boniatti <vinicioboniatti@gmail.com>
2022-10-10 08:55:16 -0300

    Atualização Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
+  domain:               'gmail.com',
   user_name:            Rails.application.credentials.gmail[:user_name].to_s,
   password:             Rails.application.credentials.gmail[:password],
   authentication:       'plain',

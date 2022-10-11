81eec1d9f2eb56e8382a520efaedba8c0bc2bf74
Vinicio Boniatti <vinicioboniatti@gmail.com>
2022-10-11 17:41:24 -0300

    att

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
   rolify
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable
+  # :lockable, :timeoutable, and :omniauthable
+
+  devise :confirmable,
+         :database_authenticatable,
+         :recoverable,
+         :registerable,
+         :rememberable,
+         :trackable,
+         :validatable
 end

f4b2c3ef5948bcef81b226d0cef7881dfa9e891f
Vinicio Boniatti <vinicioboniatti@gmail.com>
2022-10-12 11:45:25 -0300

    att

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 class ApplicationController < ActionController::Base
-    include Pundit
+  include Pundit
 
-    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  around_action :switch_locale
 
   private
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = 'You are not authorized to perform this action.'
     redirect_back(fallback_location: root_path)
   end
 end
-
